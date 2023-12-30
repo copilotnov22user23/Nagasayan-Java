b1a29de5540af31294dfa2edd9b5a2b424c8a061
copilotnov22user23 <151803854+copilotnov22user23@users.noreply.github.com>
2023-12-30 08:03:02 +0000

    Add files via upload

--- employee/src/main/java/com/employees/employee/empcontroller/employee.java
+++ employee/src/main/java/com/employees/employee/empcontroller/employee.java
@@ -6,10 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.employees.employee.empRepo.Employee;
 import com.employees.employee.empRepo.EmployeeRepo;
 
 @RestController
-public class Employee {
+public class employee {
 
     @Autowired
     private EmployeeRepo employeeRepo;
